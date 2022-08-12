ad0c9e9250583abad753e1db66b755865dc7753b
Jacob Wilson <Jacobleeeugenewilson@gmail.com>
2022-08-11 18:14:31 -0800

    add home screen

---
+++ src/screens/Home.tsx
@@ -0,0 +1,24 @@
+import { View, Text, StyleSheet, Pressable } from "react-native"
+import React from "react"
+import { useTheme } from "@react-navigation/native"
+import type { NavigatorProps } from "../utils"
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+  },
+  text: {
+    textAlign: "center",
+    marginTop: 25,
+  },
+})
+const HomeScreen = ({ navigation }: NavigatorProps) => {
+  const { colors } = useTheme()
+  return (
+    <View>
+      <Text style={[styles.text, { color: colors.text }]}>Home</Text>
+    </View>
+  )
+}
+
+export default HomeScreen
