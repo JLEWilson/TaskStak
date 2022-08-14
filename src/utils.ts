0f04659432095f38caf0c0f6b9f99d33bae73c6a
Jacob Wilson <Jacobleeeugenewilson@gmail.com>
2022-08-14 00:47:43 -0800

    moved Task logic into Task.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,16 +1,12 @@
 import type { NativeStackScreenProps } from "@react-navigation/native-stack"
 
-const TimeOfDay = {
+export const TIME_OF_DAY = {
   Morning: "Morning",
   Afternoon: "Afternoon",
   Evening: "Evening",
   Night: "Night",
 }
-type Task = {
-  description: string
-  priority?: number
-  Time?: string
-}
+
 type RootStackParamList = {
   Home: undefined
   TaskList: undefined
