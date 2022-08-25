5743e43a10f34868b1001f5ad7ec01a1dba670d8
Jacob Wilson <Jacobleeeugenewilson@gmail.com>
2022-08-25 11:20:41 -0800

    add completed bool to model

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -3,8 +3,10 @@ import A from "@react-native-async-storage/async-storage"
 export type Task = {
   id: string
   description: string
+  completed: boolean
   priority?: number
   timeOfDay?: string
+  repeating: boolean
 }
 
 export const createTask = async (task: Task): Promise<void> => {
@@ -22,6 +24,21 @@ export const getTask = async (id: Task["id"]): Promise<Task | null> => {
       return JSON.parse(result) as Task
     }
 
+    return null
+  } catch (err) {
+    throw err
+  }
+}
+
+//Since this app is only a to do list, we can just grab everything from the database
+//If I expand this app later I will abstract this further, use the keys to denote item types and use multiGet() instead
+export const getAllTasks = async () => {
+  try {
+    const keys = await A.getAllKeys()
+    const result = await A.multiGet(keys)
+    if (result && result.length >= 1) {
+      return result.map((req) => (req[1] ? (JSON.parse(req[1]) as Task) : null))
+    }
     return null
   } catch (err) {
     throw err
