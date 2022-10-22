c0f798fb1ec0d53ace3af3abea4d609a995f79bd
Jacob Wilson <Jacobleeeugenewilson@gmail.com>
2022-10-22 07:12:49 -0800

    add date formatting function

--- src/utils.ts
+++ src/utils.ts
@@ -24,3 +24,13 @@ export const addNumbers = (...args: number[]): number => {
     0,
   )
 }
+export const formatAMPM = (date: Date) => {
+  let hours = date.getHours()
+  let minutes = date.getMinutes()
+  let ampm = hours >= 12 ? "pm" : "am"
+  hours = hours % 12
+  hours = hours ? hours : 12
+  minutes = +minutes.toString().padStart(2, "0")
+  let strTime = hours + ":" + minutes + " " + ampm
+  return strTime
+}
