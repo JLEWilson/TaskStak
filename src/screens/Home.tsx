3d43890caa0f53ae9e8b3c4d25dd1d5524a65494
Jacob Wilson <Jacobleeeugenewilson@gmail.com>
2022-10-22 07:12:34 -0800

    used to view new components, reset to default

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,6 +2,7 @@ import { View, Text, StyleSheet, Pressable } from "react-native"
 import React from "react"
 import { useTheme } from "@react-navigation/native"
 import type { NavigatorProps } from "../utils"
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -14,6 +15,7 @@ const styles = StyleSheet.create({
 })
 const HomeScreen = ({ navigation }: NavigatorProps) => {
   const { colors } = useTheme()
+  const forTesting = () => {}
   return (
     <View>
       <Text style={[styles.text, { color: colors.text }]}>Home</Text>
