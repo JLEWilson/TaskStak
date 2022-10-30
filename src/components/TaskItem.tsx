006b4ca26c145a2b8d198847c674ff44cc82703a
Jacob Wilson <Jacobleeeugenewilson@gmail.com>
2022-10-29 19:35:31 -0800

    styles

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -2,24 +2,22 @@ import React from "react"
 import { View, StyleSheet, Text, Pressable } from "react-native"
 import type { Task } from "../models/Task.Server"
 import Icon from "react-native-vector-icons/MaterialIcons"
+import { useTheme } from "@react-navigation/native"
 
 const styles = StyleSheet.create({
   container: {
     marginHorizontal: 20,
     marginTop: 30,
     padding: 15,
-    backgroundColor: "#2a2847",
     borderRadius: 30,
     display: "flex",
     flexDirection: "row",
   },
   text: {
     marginLeft: 20,
-    color: "#8497cf",
     fontSize: 20,
   },
   edit: {
-    backgroundColor: "red",
     marginLeft: "auto",
     marginRight: 5,
     zIndex: 0.5,
@@ -40,10 +38,12 @@ const TaskItem: React.FC<TaskItemProps> = ({
     setTaskToEdit(task)
     setModalVisible(true)
   }
-  let test = styles.edit
+  const { colors } = useTheme()
   return (
-    <View style={styles.container}>
-      <Text style={test}>{task.description}</Text>
+    <View style={[styles.container, { backgroundColor: colors.card }]}>
+      <Text style={[styles.text, { color: colors.text }]}>
+        {task.description}
+      </Text>
       <Pressable
         style={styles.edit}
         onPress={() => {
