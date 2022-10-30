7c3a1e7c0f97d03438fe8c54292a2103bb0cff20
Jacob Wilson <Jacobleeeugenewilson@gmail.com>
2022-10-29 19:35:55 -0800

    add styles

--- src/components/WeekdaySelect.tsx
+++ src/components/WeekdaySelect.tsx
@@ -1,3 +1,4 @@
+import { useTheme } from "@react-navigation/native"
 import React, { FC } from "react"
 import { StyleSheet } from "react-native"
 import { DayPicker } from "react-native-picker-weekday"
@@ -17,6 +18,7 @@ const styles = StyleSheet.create({
 
 const WeekdaySelect: FC<WeekdaySelectProps> = ({ setWeekdayFormData }) => {
   const [weekdays, setWeekdays] = React.useState([-1])
+  const { colors } = useTheme()
   React.useEffect(() => {
     setWeekdayFormData(weekdays)
   }, [weekdays])
@@ -24,9 +26,9 @@ const WeekdaySelect: FC<WeekdaySelectProps> = ({ setWeekdayFormData }) => {
     <DayPicker
       weekdays={weekdays}
       setWeekdays={setWeekdays}
-      activeColor="violet"
-      textColor="white"
-      inactiveColor="grey"
+      activeColor={colors.primary}
+      textColor={colors.border}
+      inactiveColor={colors.background}
       itemStyles={styles.item}
       wrapperStyles={styles.wrapper}
     />
