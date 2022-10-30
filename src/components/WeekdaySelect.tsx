9cd13c27fb3e9379d49e7607e49962eb9f692253
Jacob Wilson <Jacobleeeugenewilson@gmail.com>
2022-10-29 16:11:51 -0800

    add spacing and basic styling

--- src/components/WeekdaySelect.tsx
+++ src/components/WeekdaySelect.tsx
@@ -1,15 +1,25 @@
 import React, { FC } from "react"
+import { StyleSheet } from "react-native"
 import { DayPicker } from "react-native-picker-weekday"
 
 type WeekdaySelectProps = {
   setWeekdayFormData: (days: number[]) => void
 }
 
+const styles = StyleSheet.create({
+  item: {
+    marginHorizontal: 30,
+  },
+  wrapper: {
+    marginTop: -15,
+  },
+})
+
 const WeekdaySelect: FC<WeekdaySelectProps> = ({ setWeekdayFormData }) => {
   const [weekdays, setWeekdays] = React.useState([-1])
-  // React.useEffect(() => {
-  //   setWeekdayFormData(weekdays)
-  // }, [weekdays])
+  React.useEffect(() => {
+    setWeekdayFormData(weekdays)
+  }, [weekdays])
   return (
     <DayPicker
       weekdays={weekdays}
@@ -17,6 +27,8 @@ const WeekdaySelect: FC<WeekdaySelectProps> = ({ setWeekdayFormData }) => {
       activeColor="violet"
       textColor="white"
       inactiveColor="grey"
+      itemStyles={styles.item}
+      wrapperStyles={styles.wrapper}
     />
   )
 }
