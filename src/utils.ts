502dee253a122316a0f82884c38a43218832cb0d
Jacob Wilson <Jacobleeeugenewilson@gmail.com>
2022-11-04 16:47:28 -0800

    remove unnecessary types

--- src/utils.ts
+++ src/utils.ts
@@ -1,22 +1,15 @@
 import type { NativeStackScreenProps } from "@react-navigation/native-stack"
 
-export const TIME_OF_DAY = {
-  Morning: "Morning",
-  Afternoon: "Afternoon",
-  Evening: "Evening",
-  Night: "Night",
-}
+// type RootStackParamList = {
+//   Home: undefined
+//   TaskList: undefined
+// }
 
-type RootStackParamList = {
-  Home: undefined
-  TaskList: undefined
-}
-
-export type NavigatorProps = NativeStackScreenProps<
-  RootStackParamList,
-  "Home",
-  "TaskList"
->
+// export type NavigatorProps = NativeStackScreenProps<
+//   RootStackParamList,
+//   "Home",
+//   "TaskList"
+// >
 
 export const addNumbers = (...args: number[]): number => {
   return args.reduce(
