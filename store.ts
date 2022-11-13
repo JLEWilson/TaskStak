907fb0deb756e7508db8f4783de043e9fd6a9477
Jacob Wilson <Jacobleeeugenewilson@gmail.com>
2022-11-12 22:04:42 -0900

    store connection

---
+++ store.ts
@@ -0,0 +1,18 @@
+import { configureStore } from "@reduxjs/toolkit"
+import { initialState } from "./src/reducers/ToDoListReducer"
+import RootReducer from "./src/reducers/RootReducer"
+import type { Task } from "./src/models/Task.Server"
+
+export type RootState = {
+  allTasks: Task[]
+  dailyToDoList: Task[]
+  currentTask: Task
+}
+
+const store = configureStore({
+  reducer: RootReducer,
+})
+
+export default store
+
+export type storeType = typeof store
