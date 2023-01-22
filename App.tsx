9099b7a4a10e028f237633eb0c8553aa00e5e724
Jacob Wilson <Jacobleeeugenewilson@gmail.com>
2023-01-22 14:48:04 -0900

    Add data collection on start

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import { StyleSheet } from "react-native"
+import React from "react"
 import { NavigationContainer } from "@react-navigation/native"
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import { Provider } from "react-redux"
@@ -7,6 +8,12 @@ import TaskList from "./src/screens/TaskList"
 import MaterialIcon from "react-native-vector-icons/MaterialIcons"
 import Store from "./store"
 import { useDispatch } from "react-redux"
+import {
+  getTodaysTasks,
+  getTasksForNow,
+  getAllTasks,
+} from "./src/models/Task.Server"
+import * as a from "./src/actions/ActionTypes"
 
 const MyTheme = {
   dark: true,
@@ -20,8 +27,44 @@ const MyTheme = {
   },
 }
 const Tab = createBottomTabNavigator()
+let firstLoad = true
 
 export default function App() {
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const allTasks = await getAllTasks()
+      if (allTasks === null) return
+      const dailyList = await getTodaysTasks(allTasks)
+      if (dailyList === null) return
+      const currList = getTasksForNow(dailyList)
+      const currTask = currList[0]
+
+      const action1 = {
+        type: a.setTasks,
+        payload: allTasks,
+      }
+      const action2 = {
+        type: a.setDailyToDoList,
+        payload: dailyList,
+      }
+      const action3 = {
+        type: a.setCurrentToDoList,
+        payload: currList,
+      }
+      const action4 = {
+        type: a.setCurrentTask,
+        payload: currTask,
+      }
+      Store.dispatch(action1)
+      Store.dispatch(action2)
+      Store.dispatch(action3)
+      Store.dispatch(action4)
+    }
+    if (firstLoad) {
+      firstLoad = false
+      fetchData().catch(console.error)
+    }
+  }, [])
   return (
     <Provider store={Store}>
       <NavigationContainer theme={MyTheme}>
