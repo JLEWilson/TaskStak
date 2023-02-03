9d3f1e4481b46a14ccb15e5d4b10865fa0795c49
Jacob Wilson <Jacobleeeugenewilson@gmail.com>
2023-02-03 14:41:56 -0900

    add loading to state and reducer

--- src/reducers/ToDoListReducer.ts
+++ src/reducers/ToDoListReducer.ts
@@ -1,34 +1,65 @@
 import { Task } from "../models/Task.Server"
 import { RootState } from "../../store"
 import { defaultTask } from "../models/Task.Server"
+import * as c from "../actions/ActionTypes"
+import type { Action } from "../actions/ActionTypes"
 
 export const initialState: RootState = {
+  error: null,
+  isLoadingAllTasks: false,
   allTasks: [],
+  isLoadingDaily: false,
   dailyToDoList: [],
+  isLoadingCurrent: false,
   currentToDoList: [],
   currentTask: defaultTask,
 }
-export type Action =
-  | { type: "SET_TASKS"; payload: Task[] }
-  | { type: "SET_DAILY_TO_DO_LIST"; payload: Task[] }
-  | { type: "SET_CURRENT_TO_DO_LIST"; payload: Task[] }
-  | { type: "SET_CURRENT_TASK"; payload: Task }
 
 const toDoListReducer = (state: typeof initialState, action: Action) => {
   switch (action.type) {
-    case "SET_TASKS":
+    case c.REQUEST_ALL_TASKS:
       return Object.assign({}, state, {
+        isLoading: true,
+      })
+    case c.GET_ALL_TASKS_SUCCESS:
+      return Object.assign({}, state, {
+        isLoading: false,
         allTasks: action.payload,
       })
-    case "SET_DAILY_TO_DO_LIST":
+    case c.GET_ALL_TASKS_FAILURE:
       return Object.assign({}, state, {
+        isLoading: true,
+        error: action.payload,
+      })
+    case c.REQUEST_DAILY_TO_DO_LIST:
+      return Object.assign({}, state, {
+        isLoadingDaily: true,
+      })
+    case c.GET_DAILY_TO_DO_LIST_SUCCESS:
+      return Object.assign({}, state, {
+        isLoadingDaily: false,
         dailyToDoList: action.payload,
       })
-    case "SET_CURRENT_TO_DO_LIST":
+    case c.GET_DAILY_TO_DO_LIST_FAILURE:
       return Object.assign({}, state, {
+        isLoadingDaily: false,
+        error: action.payload,
+      })
+    case c.REQUEST_CURRENT_TO_DO_LIST:
+      return Object.assign({}, state, {
+        isLoadingCurrent: true,
+      })
+    case c.GET_CURRENT_TO_DO_LIST_SUCCESS:
+      return Object.assign({}, state, {
+        isLoadingCurrent: false,
         dailyToDoList: action.payload,
       })
-    case "SET_CURRENT_TASK":
+    case c.GET_CURRENT_TO_DO_LIST_FAILURE:
+      return Object.assign({}, state, {
+        isLoadingCurrent: false,
+        dailyToDoList: action.payload,
+      })
+    case c.SET_CURRENT_TASK:
       return Object.assign({}, state, {
         currentTask: action.payload,
       })
