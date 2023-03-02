e71bcb7ee69e2be48a0b3a2953cdfa1ab2f061f0
Jacob Wilson <Jacobleeeugenewilson@gmail.com>
2023-03-02 13:37:30 -0900

    removed unnecessary redux

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,13 +1,14 @@
 import { View, Text, StyleSheet, Pressable } from "react-native"
 import React from "react"
 import { useTheme } from "@react-navigation/native"
-import type { Task } from "../models/Task.Server"
+import { getTasksForNow, getTodaysTasks, Task } from "../models/Task.Server"
 import CurrentTask from "../components/CurrentTask"
 import { passOnTask, setTaskCompleted } from "../models/Task.Server"
 import { useAppSelector, useAppDispatch } from "../hooks/redux"
 import { setCurrentTask, setCurrentTasks } from "../reducers/toDoListSlice"
 import NoTasksMessage from "../components/NoTasksMessage"
 import { RootState } from "../../store"
+import { current } from "@reduxjs/toolkit"
 
 const styles = StyleSheet.create({
   container: {
@@ -24,34 +25,39 @@ const styles = StyleSheet.create({
 })
 
 const HomeScreen = () => {
-  const currentTask = useAppSelector(
-    (state: RootState) => state.todolist.currentTask,
-  )
-  const currentToDoList = useAppSelector(
-    (state: RootState) => state.todolist.currentToDoList,
-  )
   const isLoadingAllTasks = useAppSelector(
     (state: RootState) => state.todolist.isLoadingAllTasks,
   )
-  const isLoadingCurrentTasks = useAppSelector(
-    (state: RootState) => state.todolist.isLoadingCurrent,
+  const allTasks = useAppSelector((state: RootState) => state.todolist.allTasks)
+  const currentToDoList = useAppSelector(
+    (state: RootState) => state.todolist.currentToDoList,
   )
-  const isLoadingDailyTasks = useAppSelector(
-    (state: RootState) => state.todolist.isLoadingDaily,
+  const currentTask = useAppSelector(
+    (state: RootState) => state.todolist.currentTask,
   )
   const error = useAppSelector((state: RootState) => state.todolist.error)
   const dispatch = useAppDispatch()
   const { colors } = useTheme()
 
-  const loading =
-    isLoadingAllTasks || isLoadingCurrentTasks || isLoadingDailyTasks
-
+  React.useEffect(() => {
+    console.log("useEffect 1")
+    console.log(allTasks.length)
+    if (allTasks.length > 0) {
+      dispatch(setCurrentTasks(getTasksForNow(getTodaysTasks(allTasks))))
+    }
+  }, [allTasks, dispatch])
+  React.useEffect(() => {
+    if (currentToDoList.length > 0) {
+      dispatch(setCurrentTask(currentToDoList[0]))
+    }
+  }, [currentToDoList, dispatch])
   const handleTaskCompleted = (task: Task) => {
     setTaskCompleted(task)
     const copy = [...currentToDoList]
     const tempTask = copy.shift()
     dispatch(setCurrentTasks(copy))
     dispatch(setCurrentTask(tempTask))
+    console.log("completed")
   }
   const handleTaskPassed = (task: Task) => {
     const newTaskList = passOnTask(task, [...currentToDoList])
@@ -61,7 +67,7 @@ const HomeScreen = () => {
     console.log("passed")
   }
   // now we want to conditionally render based on state
-  if (loading) {
+  if (isLoadingAllTasks) {
     return (
       <View>
         <Text>Loading...</Text>
