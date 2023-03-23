53727ca2601f34bfc16edef56d3b3681a800be1b
Jacob Wilson <Jacobleeeugenewilson@gmail.com>
2023-03-23 02:06:09 -0800

    add custom icons

--- src/components/AppNavigator.tsx
+++ src/components/AppNavigator.tsx
@@ -9,6 +9,10 @@ import { fetchAllTasks } from "../thunks/fetchData"
 import { useAppDispatch } from "../hooks/redux"
 import { StatusBar } from "react-native"
 import * as NavigationBar from "expo-navigation-bar"
+import HomeSVG from "../SVGS/HomeSVG"
+import { Colors } from "react-native/Libraries/NewAppScreen"
+import type { TransformObject } from "react-native-svg"
+import ListSVG from "../SVGS/ListSVG"
 
 const MyTheme = {
   dark: true,
@@ -26,6 +30,7 @@ const Tab = createBottomTabNavigator()
 
 const AppNavigator = () => {
   const dispatch = useAppDispatch()
+
   React.useEffect(() => {
     //This is to prevent some default behaviors that change the color
     NavigationBar.setBackgroundColorAsync("black")
@@ -49,26 +54,51 @@ const AppNavigator = () => {
           name="Home"
           children={() => <HomeScreen />}
           options={({ route }) => ({
-            tabBarIcon: () => {
+            tabBarIcon: ({ focused }) => {
               return (
-                <MaterialIcon name="home" size={25} style={{ paddingTop: 5 }} />
+                <HomeSVG
+                  height={focused ? 50 : 55}
+                  width={focused ? 50 : 55}
+                  viewBox={"-.5 -10 100 100"}
+                  stroke={MyTheme.colors.border}
+                  fill={
+                    focused
+                      ? MyTheme.colors.notification
+                      : MyTheme.colors.primary
+                  }
+                />
+                // <MaterialIcon name="home" size={25} style={{ paddingTop: 5 }} />
               )
             },
             tabBarActiveTintColor: "tomato",
             tabBarInactiveTintColor: "gray",
+            tabBarShowLabel: false,
           })}
         />
         <Tab.Screen
           name="TaskList"
           children={() => <TaskList />}
           options={({ route }) => ({
-            tabBarIcon: () => {
+            tabBarIcon: ({ focused }) => {
               return (
-                <MaterialIcon name="list" size={25} style={{ paddingTop: 5 }} />
+                <ListSVG
+                  height={focused ? 50 : 55}
+                  width={focused ? 50 : 55}
+                  strokeWidth={1.5}
+                  viewBox={"-2.5 -20 100 100"}
+                  stroke={MyTheme.colors.border}
+                  fill={
+                    focused
+                      ? MyTheme.colors.notification
+                      : MyTheme.colors.primary
+                  }
+                />
+                //<MaterialIcon name="list" size={25} style={{ paddingTop: 5 }} />
               )
             },
             tabBarActiveTintColor: "tomato",
             tabBarInactiveTintColor: "gray",
+            tabBarShowLabel: false,
           })}
         />
       </Tab.Navigator>
